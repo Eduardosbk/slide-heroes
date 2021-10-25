db7d95e9c2fbc6b8841cb1475bfa27ccc3f5b31f
Eduardo Candido <eduardo_sbk589@hotmail.com>
2021-10-25 05:44:11 -0400

    when start events we calc the distance

--- js/main.js
+++ js/main.js
@@ -2,18 +2,34 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0
+    }
+  }
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.9;
+    console.log(this.dist.movement);
+    return this.dist.finalPosition - this.dist.movement;
+  }
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
   }
   onStart(event) {
     event.preventDefault();
-    console.log('mousedown')
+    this.dist.startX = event.clientX;
+    console.log(this.dist.startX);
     this.wrapper.addEventListener('mousemove', this.onMove);
   }
   onEnd(event) {
-    console.log('acabou')
-    this.wrapper.removeEventListener('mosemove', this.onMove);
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
   }
   onMove(event) {
-    console.log('moveu')
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
   addSlideEvents() {
     this.wrapper.addEventListener('mousedown', this.onStart);
