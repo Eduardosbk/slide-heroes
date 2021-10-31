6730b6643939a6cd8523887658c3881c087e446f
Eduardo Candido <53240427+Eduardosbk@users.noreply.github.com>
2021-10-31 06:50:26 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import SlideNav from './main.js';
 
-const slide = new SlideNav('.slide', '.wrapper');
+const slide = new SlideNav('.slide','.wrapper');
 slide.init();
-slide.addArrow('.prev', '.next');
+slide.addArrow('.prev','.next');
 slide.addControl('.custom-controls');
